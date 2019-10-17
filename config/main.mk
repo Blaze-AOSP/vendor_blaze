7bcf841d8024cdd5e20aaab39f6643739f878423
dinosnore1 <dino.yash0213@gmail.com>
2019-10-17 14:59:54 +0530

    Vendor: More cleanup

--- config/main.mk
+++ config/main.mk
@@ -1,13 +1,3 @@
-# Markup libs
-ifeq ($(TARGET_BLAZE_ARCH),arm64)
-    PRODUCT_COPY_FILES += \
-           vendor/blaze/prebuilt/common/lib/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsketchology_native.so \
-           vendor/blaze/prebuilt/common/lib64/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsketchology_native.so
-else
-    PRODUCT_COPY_FILES += \
-           vendor/blaze/prebuilt/common/lib/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsketchology_native.so
-endif
-
 # Weather
 PRODUCT_COPY_FILES += \
     vendor/blaze/prebuilt/common/etc/permissions/com.android.providers.weather.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.providers.weather.xml \
