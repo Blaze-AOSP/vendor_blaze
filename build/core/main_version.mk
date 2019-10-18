68d8419c96bcc93e276fee8a1972102d3a75f1f9
dinosnore1 <dino.yash0213@gmail.com>
2019-10-18 23:05:03 +0530

    vendor: Rebase makefiles

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,10 +1,8 @@
 # blaze System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.blaze.version_number=$(BLAZE_VERSION_NUMBER) \
+    ro.blaze.version_number=$(BLAZE_REVISION) \
     ro.blaze.releasetype=$(BLAZE_BUILD_TYPE) \
     ro.blaze.version=$(BLAZE_VERSION) \
     ro.blaze.buildshort=$(BLAZE_MOD_SHORT) \
-    ro.blaze.songcodename=$(PRODUCT_VERSION_MINOR) \
-    ro.blaze.songcodeurl=$(BLAZE_SONGCODEURL) \
     ro.blaze.display.version=$(BLAZE_VERSION) \
-    ro.blaze.build.date=$(BLAZE_EPOCH)
+    ro.blaze.build.date=$(BLAZE_BUILD_DATE)
