637c06f6ab5ccc196cd3171ee22bf48db66bee50
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-23 08:35:32 -0500

    vendor: use AB_OTA_PARTITIONS flag for including backuptool_ab
    * Newer pixel devices get AB_OTA_UPDATER enabled from
      build/make/target/board/BoardConfigMainlineCommon.mk which
      is not included in time for this check.
    
    Change-Id: I1679709b7178398ffaa62ad14a6d042ff377fd5a

--- config/common.mk
+++ config/common.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
     vendor/blaze/prebuilt/common/bin/50-blaze.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-blaze.sh \
     vendor/blaze/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
 
-ifeq ($(AB_OTA_UPDATER),true)
+ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/blaze/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/blaze/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
